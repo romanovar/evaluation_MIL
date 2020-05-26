7eb29b823fdea55aae1b459267a243f2ac6c5005
romanovar <limon4e>
2020-05-26 18:39:18 +0200

    refactoring preprocessing and loading images, and splitting data set in the scripts

--- cnn/preprocessor/load_data_datasets.py
+++ cnn/preprocessor/load_data_datasets.py
@@ -11,7 +11,32 @@ def test_size_bbox(bbox_label):
     return sum_active_patches
 
 
+def split_filter_data(config, df):
+    '''
+    Splits a dataframe into test, validation and training subsets and Filters unnecessary columns
+    '''
+    results_path = config['results_path']
+    class_name = config['class_name']
+
+    print("Splitting data ...")
+
+    df_train, df_val, df_test = ld.get_train_test(df, random_state=1, do_stats=False,
+                                                  res_path=results_path,
+                                                  label_col=class_name)
+
+    label_patches = class_name + '_loc'
+    if class_name is not None:
+        df_train_filtered_cols, df_val_filtered_cols, df_test_filtered_cols = \
+            ld.keep_index_and_1diagnose_columns(df_train, label_patches), \
+            ld.keep_index_and_1diagnose_columns(df_val, label_patches), \
+            ld.keep_index_and_1diagnose_columns(df_test, label_patches)
+    return df_train_filtered_cols, df_val_filtered_cols, df_test_filtered_cols
+
+
 def load_process_xray14(config):
+    '''
+    Loads the data and the filters rows according to their class belonging
+    '''
     skip_processing = config['skip_processing_labels']
     image_path = config['image_path']
     processed_labels_path = config['processed_labels_path']
@@ -23,20 +48,7 @@ def load_process_xray14(config):
     xray_df = ld.load_xray(skip_processing, processed_labels_path, classication_labels_path, image_path,
                            localization_labels_path, results_path)
     xray_df = ld.filter_observations(xray_df, class_name, 'No Finding')
-    print(xray_df.shape)
-    print("Splitting data ...")
-
-    df_train, df_val, df_test = ld.get_train_test(xray_df, random_state=1, do_stats=False,
-                                                                   res_path = results_path,
-                                                                   label_col = class_name)
-
-    label_patches = class_name + '_loc'
-    if class_name is not None:
-        df_train_filtered_cols, df_val_filtered_cols, df_test_filtered_cols =\
-            ld.keep_index_and_1diagnose_columns(df_train, label_patches), \
-            ld.keep_index_and_1diagnose_columns(df_val,  label_patches),\
-            ld.keep_index_and_1diagnose_columns(df_test,  label_patches)
-    return df_train_filtered_cols, df_val_filtered_cols, df_test_filtered_cols
+    return xray_df
 
 
 def load_preprocess_mura(config):
@@ -64,33 +76,3 @@ def load_preprocess_pascal(config):
     pascal_image_path = config['pascal_image_path']
     df = ldp.load_pascal(pascal_image_path)
     return ldp.split_train_val_test(df)
-
-
-#TODO: delete if not used
-
-# def load_data_cv(config, data_set_name):
-#     use_xray_dataset = config['use_xray_dataset']
-#     skip_processing = config['skip_processing_labels']
-#     image_path = config['image_path']
-#     classication_labels_path = config['classication_labels_path']
-#     localization_labels_path = config['localization_labels_path']
-#     results_path = config['results_path']
-#     processed_labels_path = config['processed_labels_path']
-#
-#
-#     if use_xray_dataset:
-#         if skip_processing:
-#             xray_df = ld.load_csv(processed_labels_path)
-#             print(xray_df.shape)
-#             print('Cardiomegaly label division')
-#             print(xray_df['Cardiomegaly'].value_counts())
-#         else:
-#             label_df = ld.get_classification_labels(classication_labels_path, False)
-#             processed_df = ld.preprocess_labels(label_df, image_path)
-#             xray_df = ld.couple_location_labels(localization_labels_path, processed_df, ld.PATCH_SIZE, results_path)
-#     # print(xray_df.shape)
-#     # print("Splitting data ...")
-# #
-# # #
-# # def load_data_cv_mura():
-# #     df_train, df_val = split_data_cv(df, splits_nr, current_split, random_seed, diagnose_col, ratio_to_keep=None)
